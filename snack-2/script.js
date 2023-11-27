6af5f40dc5888415ba90cb1f467c7e9f74282af4
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-27 12:26:39 +0100

    errore nella creazione del nuovo array. Non prende la arrow function

---
+++ snack-2/script.js
@@ -0,0 +1,45 @@
+// Crea un array di oggetti che rappresentino delle persone.
+// Ogni persona ha un nome, un cognome e un'età.
+
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome 
+// e l'indicazione se può guidare, in base all'età.
+
+const listaPersone = [
+   {
+      nome: 'Gino',
+      cognome: 'Ajeje',
+      eta: 23
+   },
+
+   {
+      nome: 'Gina',
+      cognome: 'Brazof',
+      eta: 17
+   },
+   {
+      nome: 'Robby',
+      cognome: 'Castro',
+      eta: 57
+   },
+   {
+      nome: 'Ugo',
+      cognome: 'Costrutto',
+      eta: 14
+   },
+   {
+      nome: 'Pina',
+      cognome: 'Abete',
+      eta: 29
+   },
+   {
+      nome: 'Carlo',
+      cognome: 'Rossi',
+      eta: 12
+   }
+];
+
+const nuovaListaNomi = listaPersone.map (numero) => {
+   
+}
+
+console.log(nuovaListaNomi)
