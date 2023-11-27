030d2649ee322eebee09f83e5d2d32b2787187c8
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-27 11:38:44 +0100

    creato array di oggetti

---
+++ snack-1/script.js
@@ -0,0 +1,70 @@
+// Crea un array composto da 10 automobili.
+// Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+
+// Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+ 
+// Infine stampa separatamente i 3 array.
+
+
+const macchine = [
+ {
+    marca: 'gx225sw',
+    modello: 'ford',
+    alimentazione:'benzina'
+ },
+
+ {
+    marca: 'fe231ds',
+    modello: 'ferrari',
+    alimentazione:'gpl'
+ },
+
+ {
+    marca: 'gq112dq',
+    modello: 'lamborghini',
+    alimentazione:'diesel'
+ },
+
+ {
+    marca: 'gx225sw',
+    modello: 'ford',
+    alimentazione:'gpl'
+ },
+
+ {
+    marca: 'gx225sw',
+    modello: 'ford',
+    alimentazione:'benzina'
+ },
+
+ {
+    marca: 'gx225sw',
+    modello: 'ford',
+    alimentazione:'benzina'
+ },
+
+ {
+    marca: 'gx225sw',
+    modello: 'ford',
+    alimentazione:'diesel'
+ },
+
+ {
+    marca: 'gx225sw',
+    modello: 'ford',
+    alimentazione:'metano'
+ },
+
+ {
+    marca: 'gx225sw',
+    modello: 'ford',
+    alimentazione:'gpl'
+ },
+
+ {
+    marca: 'gx225sw',
+    modello: 'ford',
+    alimentazione:'benzina'
+ }
+
+];
